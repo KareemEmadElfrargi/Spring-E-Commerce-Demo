8baa53824ccd81ab1e0a39de917ea4799340f9a6
Kareem Emad <@gmail.com>
2025-05-08 15:29:29 +0300

    Initial commit

---
+++ src/test/java/com/kareem/Spring_E_Commerce_Demo/SpringECommerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kareem.Spring_E_Commerce_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
